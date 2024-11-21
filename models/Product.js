c90eb3a52e3bbc55c15cf5516cab0e6e96a5f1b7
aashish shrestha <as.aashish345@gmail.com>
2024-11-21 13:13:31 +0545

    project setup

--- models/Product.js
+++ models/Product.js
@@ -1,46 +1,45 @@
 import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema(
-    {
-      product_name: {
-        type: String,
-        required: true,
-      },
-      product_detail: {
-        type: String,
-        required: true,
-      },
-      product_price: {
-        type: Number,
-        required: true,
-      },
-      rating: {
-        type: Number,
-        default: 0,
-      },
-      numReviews: {
-        type: Number,
-        default: 0,
-      },
-  
-      product_image: {
-        type: String,
+  {
+    product_name: {
+      type: String,
       required: true,
-      },
-      product_collection: {
-        type: String,
-        required: true,
-      },
-      category: {
-        type: String,
-        required: true,
-      },
-      countInStock: {
-        type: Number,
-        required: true,
-      },
-      reviews: [reviewSchema],
     },
-    { timestamps: true }
-  );
-  module.exports = mongoose.model("Product", productSchema);
+    product_detail: {
+      type: String,
+      required: true,
+    },
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+    product_image: {
+      type: String,
+      required: true,
+    },
+    product_collection: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+    reviews: [reviewSchema],
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Product", productSchema);
